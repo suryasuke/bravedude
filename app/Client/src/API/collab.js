0b2f9285a28d155b3360a8188f582c9e826f8d5c
SURYA SHANMUGAM <138099237+suryasuke@users.noreply.github.com>
2026-06-04 18:11:50 +0530

    Update baseURL to new server address

--- app/Client/src/API/collab.js
+++ app/Client/src/API/collab.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = 'http://localhost:4000/collab'
+const baseURL = 'http://16.171.14.101:4000/collab'
 
 export const submitCollab = async (data) => {
     console.log(data , "in API");
